8be320b7c6dbd4752ec73bcc8cdb1a2b01ba3861
amit06lather <96193598+amit06lather@users.noreply.github.com>
2021-12-16 17:35:48 +0530

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+<?php
+include("connection.php");
+error_reporting(0);
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -76,12 +81,12 @@
             <div class="form-group">
               <label for="exampleInputEmail1">Email address</label>
               <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                placeholder="Enter email">
+                placeholder="Enter email" name="email">
               <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div class="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              <label for="exampleInputPassword">Password</label>
+              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
             </div>
             <div class="form-check">
               <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -281,3 +286,19 @@
 </body>
 
 </html>
+
+<?php
+
+$firstname=$_GET['email'];
+$lastname=$_GET['password'];
+
+$query="INSERT INTO LOGIN VALUES ('$email','$password')";
+$data=mysqli_query($conn,$query);
+
+if ($data) {
+    echo "DATA INSERTED INTO DATABASE";
+}
+else {
+    "Failed to insert into database";
+}
+?>
